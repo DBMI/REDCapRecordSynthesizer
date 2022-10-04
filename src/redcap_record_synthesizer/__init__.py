16c9da44d93832b56ec893b7ef3515de5d70ffcd
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-04 12:31:04 -0700

    Synch version numbers.

--- src/redcap_record_synthesizer/__init__.py
+++ src/redcap_record_synthesizer/__init__.py
@@ -5,7 +5,7 @@ Class FakeRecordGenerator, which lets us synthesize
 patient records with realistic data.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.2.1"
 __author__ = "Kevin J. Delaney"
 __credits__ = (
     "University of California San Diego"
