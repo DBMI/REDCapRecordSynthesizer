445bfafc6ca5cc3175687871f6cf0e53b8e1bef9
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-10-04 14:11:45 -0700

    Show where we're looking for the names.csv file.

--- src/redcap_record_synthesizer/__init__.py
+++ src/redcap_record_synthesizer/__init__.py
@@ -5,11 +5,4 @@ Class FakeRecordGenerator, which lets us synthesize
 patient records with realistic data.
 """
 
-__version__ = "0.2.3"
-__author__ = "Kevin J. Delaney"
-__credits__ = (
-    "University of California San Diego"
-    + "School of Medicine; Department of Biomedical Informatics"
-)
-
 from .fake_records import FakeRecordGenerator
