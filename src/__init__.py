65b91909a358d1efe85293e60db39db7eec400bc
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-01-12 11:22:51 -0800

    Switch back to delivering just one package.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 """
 Source code
 """
-import nickname_lookup  # type: ignore[import]  # noqa: F401
 import redcap_record_synthesizer  # type: ignore[import]  # noqa: F401
--- src/redcap_record_synthesizer/__init__.py
+++ src/redcap_record_synthesizer/__init__.py
@@ -4,3 +4,4 @@ REDCap Record Synthesizer
 Class FakeRecordGenerator, which lets us synthesize
 patient records with realistic data.
 """
+import nickname_lookup  # type: ignore[import]  # noqa:F401
