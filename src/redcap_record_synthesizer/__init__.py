8e7fdf0385e2bd6a24e0db46d1121a9a56b7f342
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-01-17 15:49:34 -0800

    Fixing pipeline failures.

--- src/redcap_record_synthesizer/__init__.py
+++ src/redcap_record_synthesizer/__init__.py
@@ -5,6 +5,7 @@ Class FakeRecordGenerator, which lets us synthesize
 patient records with realistic data.
 """
 import nickname_lookup  # type: ignore[import]  # noqa:F401
+
 from .fake_records import FakeRecordGenerator  # type: ignore[import]  # noqa:F401
 from .state_abbr_conversion import (  # type: ignore[import]  # noqa:F401
     StateAbbreviationConverter,
